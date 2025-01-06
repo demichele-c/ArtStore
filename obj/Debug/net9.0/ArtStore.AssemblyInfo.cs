4cdca836c07066788cbfe3c065ce17f2f719df8e
demichele-c <demichele.charles@yahoo.com>
2025-01-06 11:28:48 -0500

    needs to be merged

--- obj/Debug/net9.0/ArtStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/ArtStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1342c18821564b370c4a1e4d7a26028012ed505")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
